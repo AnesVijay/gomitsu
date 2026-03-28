0ab36169683ac91ba26c20efbc3550f9ddf53b90
AnesVijay <2011andreyka@gmail.com>
2026-03-28 15:37:09 +0500

    TODO

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ var (
 	botToken = getToken()
 )
 
+// TODO
+// 1. переписать бота с tgbotapi на telegram-bot (там удобнее через хендлеры настраивать поведение бота) или создать map[string]func(), для красивой инициализации команд
+// 2. db:
+// 2.1 бот должен сам себе создать нужные базы (chatID будет уникальным ключом)
+// 2.2 бот должен сам задать туда администратора (его chatID берётся из конфига)
+
+// пример обработки сообщения через эту либу: https://github.com/go-telegram-bot-api/telegram-bot-api/blob/master/docs/examples/command-handling.md
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
@@ -26,10 +34,10 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		// TODO: сделать что-то в духе switch-case
+		// TODO: сделать что-то в духе switch-case => map[string]func()
 		// TODO: нужны сценарии
 		//?? - регистрация (через моё одобрение по inline-клавиатуре, которая удаляется после принятия или отказа -> сообщение отмечает как лог моё действие)
-		//?? - отправка сообщений челикам из БД (общая отправка всем и только одному, когда это ссылка на подключение)
+		//?? - отправка сообщений челикам из БД (общая отправка всем или только одному, когда это ссылка на подключение)
 
 		if update.Message != nil {
 			log.Printf("[%s (ID: %d)]: %s", update.Message.From.UserName, update.Message.From.ID, update.Message.Text)
